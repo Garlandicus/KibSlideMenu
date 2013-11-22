acf2628473166ffc293d1a5cb2b42671debcc718
Ryan Darge <ryan.darge@gmail.com>
2013-11-22 18:55:23 -0500

    More small changes
    Last fix of the day, probably.

--- assets/android.js
+++ assets/android.js
@@ -1,5 +1,7 @@
 //Adapter methods for the slideshow program
 
+
+//If used on an android-based web browser
 function showAndroidToast(toast) {
 	if (navigator.userAgent.indexOf('Chrome') != -1 && parseFloat(navigator.userAgent.substring(navigator.userAgent.indexOf('Chrome') + 7).split(' ')[0]) >= 15){//Chrome
 	       //do nothing
